951c1a593442978c85aef94042d8b1e0799b49ab
Ben Galloway <bdgalloway85@gmail.com>
2020-10-04 00:02:14 -0500

    started script file, add jQuery link

---
+++ script.js
@@ -0,0 +1,28 @@
+//Timer
+//Score
+//Highscores with player names
+
+//question bank
+
+$(document).ready(function() {
+
+//main page
+$('#start-button').on('click', function() {
+alert('click!');
+});
+
+//remove main items
+
+//add first question and start timer
+
+//second question
+
+//third, fourth, etc.
+
+//All done! Your final score is:  Enter initials
+
+//highscores page
+
+
+
+});
